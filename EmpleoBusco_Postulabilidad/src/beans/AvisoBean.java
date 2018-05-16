1f70c0827e94bf38a9f96ec337cb940d7b2d1e92
dtransforma <giancarlo.inga.c@gmail.com>
2018-05-16 12:55:38 -0500

    Instalando conexiones

--- EmpleoBusco_Postulabilidad/src/beans/AvisoBean.java
+++ EmpleoBusco_Postulabilidad/src/beans/AvisoBean.java
@@ -13,6 +13,63 @@ package beans;
 public class AvisoBean {
     
    int id_empresa,id_aviso; 
+   String nombre_empresa,pais,fecha;
+   int postulaciones;
+
+    public AvisoBean() {
+        
+      postulaciones=0;
+        
+    }
+
+    public int getId_empresa() {
+        return id_empresa;
+    }
+
+    public void setId_empresa(int id_empresa) {
+        this.id_empresa = id_empresa;
+    }
+
+    public int getId_aviso() {
+        return id_aviso;
+    }
+
+    public void setId_aviso(int id_aviso) {
+        this.id_aviso = id_aviso;
+    }
+
+    public String getNombre_empresa() {
+        return nombre_empresa;
+    }
+
+    public void setNombre_empresa(String nombre_empresa) {
+        this.nombre_empresa = nombre_empresa;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getPostulaciones() {
+        return postulaciones;
+    }
+
+    public void setPostulaciones(int postulaciones) {
+        this.postulaciones = postulaciones;
+    }
+   
     
     
 }
