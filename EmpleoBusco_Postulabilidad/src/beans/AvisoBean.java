81083d53a731409567abcc6d8f279960edf0de0b
dtransforma <giancarlo.inga.c@gmail.com>
2018-05-16 12:35:42 -0500

    iniciando

---
+++ EmpleoBusco_Postulabilidad/src/beans/AvisoBean.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package beans;
+
+/**
+ *
+ * @author user
+ */
+public class AvisoBean {
+    
+   int id_empresa,id_aviso; 
+    
+    
+}
